ff0f10537456e9a2ca6dd71671cc4e4524d5eb0b
asmsulaiman89 <asmsulaiman89@gmail.com>
2025-04-15 17:16:32 +0600

    backend complete

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import {
+  addUserRating,
+  getUserCourseProgress,
   getUserData,
   purchaseCourse,
+  updateUserCourseProgress,
   userEnrolledCourses,
 } from "../controllers/userController.js";
 
@@ -11,5 +14,9 @@ userRouter.get("/data", getUserData);
 
 userRouter.get("/enrolled-courses", userEnrolledCourses);
 userRouter.post("/purchase", purchaseCourse);
+
+userRouter.post("update-course-progress", updateUserCourseProgress);
+userRouter.post("get-course-progress", getUserCourseProgress);
+userRouter.post("add-rating", addUserRating);
 
 export default userRouter;
